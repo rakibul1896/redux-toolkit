37f789efe97acfa951a4726a9efe5426b03ee3df
rakibul1896 <rhkhan9618@gmail.com>
2022-03-09 19:17:35 +0600

    cleanUp

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import './Index.css';
 import App from './App';
-import store from './store';
 
-// console.log(store.getState())
-// const unsubscribe = store.subscribe(() =>
-//   console.log('State after dispatch: ', store.getState())
-// )
-// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-// store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-
-// store.dispatch({
-//   type: 'filters/colorFilterChanged',
-//   payload: { color: 'red', changeType: 'added' }
-// })
-// unsubscribe();
-
-// store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
