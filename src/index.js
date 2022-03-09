9bfb2b3ae7afe4edf28e9978fa7cbb59b688746a
rakibul1896 <rhkhan9618@gmail.com>
2022-03-09 21:25:42 +0600

    @reduxjs/toolkit setup completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './Index.css';
 import App from './App';
-
+import store from './app/sotre';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
