c841f78cf349fb210e612865e81266dc0ea31d64
rakibul1896 <rhkhan9618@gmail.com>
2022-03-12 21:32:51 +0600

    remove some file from root file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './Index.css';
+import './index.css';
 import App from './App';
 import store from './app/sotre';
 
